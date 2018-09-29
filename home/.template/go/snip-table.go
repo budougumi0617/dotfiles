3e13f51668b51d251f5bd340b4d2ff1a668d6c55
budougumi0617 <budougumi0617@gmail.com>
2018-09-29 16:44:37 +0900

    :arrow_up: Update template

--- home/.template/go/snip-table.go
+++ home/.template/go/snip-table.go
@@ -2,15 +2,30 @@
 		subject string
 		{{_cursor_}}
 		want  string
+		wantError bool
+		err       error
 	}{
 		{
 			subject: "",
-			got: "",
+			want: "",
+			wantError: true,
+			// err: fmt.Errorf(""),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.subject, func(t *testing.T) {
 			got := ""
+
+			if !tt.wantError && err != nil {
+				t.Fatalf("want no err, but has error %#v", err)
+			}
+			if tt.wantError && !reflect.DeepEqual(err, tt.err) {
+				t.Fatalf("want %#v, but %#v", tt.err, err)
+			}
+			// if !tt.wantError && got != tt.want {
+			// 	t.Fatalf("want %q, but %q", tt.want, got)
+			// }
+
 			if s != tt.want {
 				t.Errorf("want %#v, but got %#v", tt.want, got)
 			}
