62c8cfcba5fbf3b9eb734919a8969a1838e23c63
budougumi0617 <budougumi0617@gmail.com>
2023-11-23 23:20:12 +0900

    feat(nvim): 総体行数の無効化

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- 相対行数の表示を無効化
+vim.wo.relativenumber = false
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
