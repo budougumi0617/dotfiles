55f70c4a355d3434a41f937e9c313d5d7827462a
budougumi0617 <budougumi0617@gmail.com>
2024-07-07 08:10:27 +0900

    feat(nvim): VSCodeでは読みこまない

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,2 +1,9 @@
+-- VSCode Neovimとの使いわけ
+if vim.g.vscode == 1 then
+  -- require("config.vscode")
+  -- https://zenn.dev/r57ty7/articles/accd3014e22366
+  return
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
