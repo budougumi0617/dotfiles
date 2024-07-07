6198af0c1cef9b4815b40ba4992db1504dd7690a
budougumi0617 <budougumi0617@gmail.com>
2024-07-07 10:37:30 +0900

    feat(nvim): GraphQL関係の設定を追加

---
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "graphql",
+        "markdown",
+        "markdown_inline",
+        "tsx",
+        "typescript",
+        "yaml",
+      },
+    },
+  },
+}
