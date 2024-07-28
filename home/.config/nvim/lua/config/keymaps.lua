f01393388fb4a8d65cee598c1b2e79e73fc0f8f3
budougumi0617 <budougumi0617@gmail.com>
2024-07-28 16:37:57 +0900

    chore(nvim): ALTキーを使った設定について確認

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,9 @@ local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 keymap.set("i", "jj", "<Esc>", opts)
+-- Altキーを使ったキーマップを使うときはiterm2の設定を変えておく必要がある
+-- https://qiita.com/DQNEO/items/524724982ef49beb7f56
+-- iTerm2のPreference -> Profiles -> keys の設定画面で、\
+-- Left Option key acts as +Esc にチェックjj
+--
+-- keymap.set("n", "<A-z>", "<cmd>FzfLua live_grep<cr>", opts)
