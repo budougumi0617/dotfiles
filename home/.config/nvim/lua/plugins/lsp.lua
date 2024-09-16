1ad17ed92fe20919757c3d5fe8109189c91dd7d0
budougumi0617 <budougumi0617@gmail.com>
2024-09-16 16:05:41 +0900

    fix(nvim): ignore key map

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -12,8 +12,18 @@ return {
         "tailwindcss-language-server",
         "typescript-language-server",
         "css-lsp",
-        "graphql-language-service-cli"
+        "graphql-language-service-cli",
       })
     end,
   },
+  -- NOTE: custom key config
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = function()
+  --     local keys = require("lazyvim.plugins.lsp.keymaps").get()
+  --     -- add a keymap
+  --     keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
+  --   end,
+  -- }
 }
+
