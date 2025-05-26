cae084067807f17187d8f16742f47d6f4dd6517e
budougumi0617 <budougumi0617@gmail.com>
2025-05-27 06:26:34 +0900

    feat(nvim): pkmリポジトリの日記メモの場所を変更

--- home/.config/nvim/lua/plugins/obsidian.lua
+++ home/.config/nvim/lua/plugins/obsidian.lua
@@ -39,6 +39,18 @@ return {
         path = "~/vaults/budougumi0617/pkm",
         overrides = {
           notes_subdir = "01_fleetings",
+          daily_notes = {
+            -- Optional, if you keep daily notes in a separate directory.
+            folder = "01_fleetings",
+            -- Optional, if you want to change the date format for the ID of daily notes.
+            date_format = "%Y-%m-%d",
+            -- Optional, if you want to change the date format of the default alias of daily notes.
+            alias_format = "%B %-d, %Y",
+            -- Optional, default tags to add to each new daily note created.
+            default_tags = { "daily" },
+            -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+            template = "daily_template.md",
+          },
         },
       },
     },
