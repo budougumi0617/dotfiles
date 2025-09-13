160eb1c02cc2edca06e2e9de512bf8f7e7637c51
budougumi0617 <budougumi0617@gmail.com>
2025-09-13 18:17:56 +0900

    fix(nvim): deprecated

--- home/.config/nvim/lua/plugins/obsidian.lua
+++ home/.config/nvim/lua/plugins/obsidian.lua
@@ -102,31 +102,32 @@ return {
       min_chars = 2,
     },
 
+    -- deprecated
     -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
     -- way then set 'mappings = {}'.
-    mappings = {
-      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-      ["gf"] = {
-        action = function()
-          return require("obsidian").util.gf_passthrough()
-        end,
-        opts = { noremap = false, expr = true, buffer = true },
-      },
-      -- Toggle check-boxes.
-      ["<leader>ch"] = {
-        action = function()
-          return require("obsidian").util.toggle_checkbox()
-        end,
-        opts = { buffer = true, desc = "Toggle checkbox" },
-      },
-      -- Smart action depending on context, either follow link or toggle checkbox.
-      ["<cr>"] = {
-        action = function()
-          return require("obsidian").util.smart_action()
-        end,
-        opts = { buffer = true, expr = true },
-      },
-    },
+    --  mappings = {
+    --    -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+    --    ["gf"] = {
+    --      action = function()
+    --        return require("obsidian").util.gf_passthrough()
+    --      end,
+    --      opts = { noremap = false, expr = true, buffer = true },
+    --    },
+    --    -- Toggle check-boxes.
+    --    ["<leader>ch"] = {
+    --      action = function()
+    --        return require("obsidian").util.toggle_checkbox()
+    --      end,
+    --      opts = { buffer = true, desc = "Toggle checkbox" },
+    --    },
+    --    -- Smart action depending on context, either follow link or toggle checkbox.
+    --    ["<cr>"] = {
+    --      action = function()
+    --        return require("obsidian").util.smart_action()
+    --      end,
+    --      opts = { buffer = true, expr = true },
+    --    },
+    --  },
 
     -- Where to put new notes. Valid options are
     --  * "current_dir" - put new notes in same directory as the current buffer.
